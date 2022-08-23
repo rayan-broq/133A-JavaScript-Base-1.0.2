3097c87c80ed37ab21b1c76ee33795f0b4b72b41
rayan-broq <94693010+rayan-broq@users.noreply.github.com>
2022-08-23 14:46:27 +0200

    Initial commit

---
+++ 133A-JavaScript-Base-1.0.2/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author  Rayan Broquet
+ * @version 0.2
+ * @since   2022-08-23
+ */
+
+(function main() {
+  'use strict'; // Demande une interprétation stricte du code
+
+    let a = 'Bonjour';
+    let b = 'toto';
+
+    console.log(a, b);
+}()); // Main IIFE
+
